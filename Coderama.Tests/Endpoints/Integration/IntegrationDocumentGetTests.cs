c69d46cd8a9b1f90ca005ff92a5a7f407144b6c8
Slavomir Danas <slavomir.danas@outlook.com>
2026-03-31 14:05:46 +0200

    Tests - Add Endpoint integration tests

---
+++ Coderama.Tests/Endpoints/Integration/IntegrationDocumentGetTests.cs
@@ -0,0 +1,77 @@
+namespace Coderama.Tests.Endpoints.Integration;
+
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http.Json;
+
+public class IntegrationDocumentGetTests : IClassFixture<WebApplicationFactory<Program>> {
+
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public IntegrationDocumentGetTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task DocumentGet_ShouldReturnDocument()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync($"/documents/${Guid.NewGuid()}");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        response.Content.Headers.ContentType.ShouldNotBeNull();
+        response.Content.Headers.ContentType.MediaType.ShouldBe("application/json");
+    }
+
+    [Fact]
+    public async Task DocumentPost_ShouldReturnCreated()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var payload = new {
+            id = "some-unique-identifier1",
+            tags = (string[])["important", ".net"],
+            data = new {
+                some = "data",
+                optional = "fields"
+            }
+        };
+
+        // Act
+        var response = await client.PostAsJsonAsync("/documents", payload);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        response.StatusCode.ShouldBe(HttpStatusCode.Created);
+        response.Content.Headers.ContentType.ShouldNotBeNull();
+        response.Content.Headers.ContentType.MediaType.ShouldBe("application/json");
+    }
+
+    [Fact]
+    public async Task DocumentPut_ShouldReturnOk()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var payload = new {
+            id = "some-unique-identifier1",
+            tags = (string[])["important", ".net"],
+            data = new {
+                some = "data",
+                optional = "fields"
+            }
+        };
+
+        // Act
+        var response = await client.PutAsJsonAsync($"/documents/${Guid.NewGuid()}", payload);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+    }
+}
