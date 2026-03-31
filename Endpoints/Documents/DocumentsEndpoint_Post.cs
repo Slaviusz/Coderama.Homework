99b187be18f7db91b2ba6c875ef6bd84b6f8b1f1
Slavomir Danas <slavomir.danas@outlook.com>
2026-03-31 11:28:37 +0200

    Add API Endpoints

---
+++ Endpoints/Documents/DocumentsEndpoint_Post.cs
@@ -0,0 +1,39 @@
+namespace Coderama.Homework.Endpoints.Documents;
+
+using Contracts.Validators;
+
+file class DocumentsEndpoint {
+    public static void Post(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/documents",
+            async (
+                DocumentPostRequest request,
+                ILogger<DocumentsEndpoint> logger,
+                IDocumentStorage documentStorage,
+                CancellationToken cancellationToken
+            ) => {
+                logger.LogInformation($"Processing POST Request");
+
+                var storeResult = await documentStorage.StoreDocumentAsync(request.Id, request.Data, cancellationToken);
+
+                return storeResult.Match<IResult>(
+                    created => TypedResults.CreatedAtRoute(
+                        routeName: "GetDocumentById",
+                        routeValues: new { internalId = created.Value },
+                        value: new DocumentPostCreatedResponse { InternalId = created.Value }
+                    ),
+                    error => TypedResults.Problem(
+                        detail: error.Value,
+                        statusCode: StatusCodes.Status500InternalServerError)
+                    );
+            })
+            .AddEndpointFilter<ValidationFilter<DocumentPostRequest>>()
+            .WithName("AddDocument")
+            .WithTags("Documents")
+            .WithDescription("Add new document")
+            .ProducesProblem(StatusCodes.Status500InternalServerError)
+            .Produces<DocumentPostCreatedResponse>(StatusCodes.Status201Created)
+            .Produces<BadRequestObjectResult>(StatusCodes.Status400BadRequest)
+            ;
+    }
+}
---
+++ Endpoints/Documents/DocumentsEndpoint_Put.cs
@@ -0,0 +1,46 @@
+namespace Coderama.Homework.Endpoints.Documents;
+
+using Contracts.Validators;
+
+file class DocumentsEndpoint {
+    public static void Put(IEndpointRouteBuilder app)
+    {
+
+        app.MapPut("/documents/{internalId}",
+            async (
+                string internalId,
+                DocumentPutRequest request,
+                ILogger<DocumentsEndpoint> logger,
+                IDocumentStorage documentStorage,
+                CancellationToken cancellationToken
+            ) => {
+                logger.LogInformation($"Processing PUT Request");
+
+                var getResult = await documentStorage.GetDocumentByIdAsync(internalId, cancellationToken);
+
+                return await getResult.Match<Task<IResult>>(
+                    async document => {
+                        var updateResult = await documentStorage.UpdateDocumentAsync(document.Value.InternalId, request.Data, cancellationToken);
+
+                        return updateResult.Match<IResult>(
+                            success => TypedResults.Ok(),
+                            error => TypedResults.Problem(
+                                detail: error.Value,
+                                statusCode: StatusCodes.Status500InternalServerError
+                            )
+                        );
+                    },
+                    async notfound => TypedResults.NotFound()
+                );
+            })
+            .AddEndpointFilter<ValidationFilter<DocumentPutRequest>>()
+            .WithName("UpdateDocumentById")
+            .WithTags("Documents")
+            .WithDescription("Update existing document by its Id")
+            .Produces(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status500InternalServerError)
+            .Produces<DocumentPostCreatedResponse>(StatusCodes.Status201Created)
+            .Produces<BadRequestObjectResult>(StatusCodes.Status400BadRequest)
+            ;
+    }
+}
